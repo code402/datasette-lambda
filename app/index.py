bb2cd5ba1de76b2eb9f0e7dc4ab2da2ce6e5df73
Colin Dellow <cldellow@gmail.com>
2020-04-09 22:12:10 -0400

    Pass stage component in event['path']
    I'm not clear enough on ASGI to know if this is (1) a bug in Mangum,
    (2) a bug in Datasette, or (3) if the spec is open to interpretation such
    that both implementations are compliant (yet non-interoperable).
    
    I lean towards perhaps a little of (2) and (3).
    
    For Mangum - in the `handle_http` [1] case, it doesn't use the `stageName` at all.
    
    In the `handle_ws` [2] case, it passes the `stageName` as the
    `root_path`, which the WSGI spec defines as:
    
    > The root path this application is mounted at; same as SCRIPT_NAME in
    > WSGI. Optional; defaults to "".
    
    For Datasette - it does not appear to use the ASGI concept of root_path
    at all.
    
    [1]: https://github.com/erm/mangum/blob/566b0b7569459a8ef6463af08780300521ae61b1/mangum/adapter.py#L87
    [2]: https://github.com/erm/mangum/blob/566b0b7569459a8ef6463af08780300521ae61b1/mangum/adapter.py#L132

--- app/index.py
+++ app/index.py
@@ -34,5 +34,18 @@ ds = Datasette(
     version_note=None #version_note,
 )
 app = ds.app()
+handler_ = Mangum(app)
 
-handler = Mangum(app)
+def handler(event, context):
+    # path doesn't include the stage component, but Datasette requires it in order to
+    # correctly construct URLs in some cases.
+    #
+    # See https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format for description of the format
+    # of this object.
+    #
+    # See https://github.com/simonw/datasette/issues/394#issuecomment-603501719 for where
+    # simonw identifies that the full path must be present.
+
+    event['path'] = '/' + event['requestContext']['stage'] + event['path']
+    return handler_(event, context)
+
